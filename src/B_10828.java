825c2a22509e4d08f0d3f135bcfc43c6ab18fd6b
pdh903 <92020585+pdh903@users.noreply.github.com>
2023-12-04 14:52:26 +0900

    commit

---
+++ src/B_10828.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class B_10828 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < n; i++) {
+            String[] tokens = br.readLine().split(" ");
+
+            switch (tokens[0]) {
+                case "push":
+                    int num = Integer.parseInt(tokens[1]);
+                    stack.push(num);
+                    break;
+                case "pop":
+                    if (stack.isEmpty()) {
+                        System.out.println(-1);
+                    } else {
+                        System.out.println(stack.pop());
+                    }
+                    break;
+                case "size":
+                    System.out.println(stack.size());
+                    break;
+                case "empty":
+                    if (stack.isEmpty()) {
+                        System.out.println(1);
+                    } else {
+                        System.out.println(0);
+                    }
+                    break;
+                case "top":
+                    if (stack.isEmpty()) {
+                        System.out.println(-1);
+                    } else {
+                        System.out.println(stack.peek());
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
